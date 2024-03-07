06c53e92ade83ebfbee53159013d6ce9c51c55ee
Tri Tran <trith13102@gmail.com>
2024-03-08 02:23:54 +0700

    :sparkles: Implement App Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      base: "0em",
+      sm: "30em",
+      md: "48em",
+      lg: "62em",
+      xl: "80em",
+      "2xl": "96em",
+    },
     extend: {
       colors: {
         primary: "#2947a9",
