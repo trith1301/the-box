4d8354c42a02b4285d20598c6f268c2e0bc12fdc
Tri Tran <trith13102@gmail.com>
2024-03-08 19:39:25 +0700

    :sparkles: Implement Advertisement section

---
+++ src/components/shared/Advertisement.tsx
@@ -0,0 +1,34 @@
+import { Container } from "@chakra-ui/react"
+
+const Advertisement = () => {
+  return (
+    <section className="relative h-[300px] bg-center bg-cover bg-[url('/images/advertisement/thumbnail.png')]">
+      <div className="z-[2] absolute top-0 left-0 right-0 w-full h-full">
+        <Container
+          className="flex flex-col lg:flex-row items-center justify-center lg:justify-between"
+          maxW={["95%", "95%", "95%", "95%", "1200px"]}
+          height="100%"
+        >
+          <div className="flex flex-col gap-[10px] lg:gap-[30px] pb-[50px] lg:pb-unset">
+            <h3 className="md:text-[24px] lg:text-[36px] text-center lg:text-left font-bold text-white">
+              Free consultation with exceptional quality
+            </h3>
+            <p className="lg:text-[24px] text-center lg:text-left text-white">
+              Just one call away:{" "}
+              <span className="underline">+84 1102 2703</span>
+            </p>
+          </div>
+          <button
+            className="md:px-[12px] md:py-[6px] lg:px-[20px] lg:py-[16px] border-2 border-[#fffff] rounded-[2px] md:text-[14px] lg:text-[18px] text-white hover:text-black hover:bg-white"
+            type="button"
+          >
+            Get your consultation
+          </button>
+        </Container>
+      </div>
+      <div className="z-[1] absolute top-0 left-0 right-0 w-full h-full bg-black opacity-70"></div>
+    </section>
+  )
+}
+
+export default Advertisement
